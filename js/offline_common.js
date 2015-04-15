b4048652a7932ce55debbf5b84edb64d9b7ac614
devmyk <dev.my.kim@gmail.com>
2015-04-15 11:56:51 +0900

    merge with offline source

---
+++ js/offline_common.js
@@ -0,0 +1,135 @@
+/*
+var data = [];
+var arr = [];
+var sc = [];
+var mode = "words"; // full, words
+var list = "";
+var sum = 0;
+var curr = 0;
+var max = 10;
+var sm;
+var timeout;
+*/
+
+var isRecycle = 0;
+var startSeq = 0;
+var path = "";
+var _js = "data/1001/ch01.js";
+var isMobile = isMobile();
+var autoplay = (isMobile ? 0 : 1);
+var data = [];
+
+function save_db()
+{
+	var f = document.f2;
+	var e = document.getElementById('send_data');
+	var sends = "";
+	if (data.length == 0 || code == "") {
+		document.iframe.document.body.innerHTML = "empty data";
+		return;
+	} else {
+		f.code.value = code;
+		// seq, corr, answer
+		for(var i=0; i < data.length; i++){
+			if (data[i].try == 1) {
+				var cr = (data[i].correct ? "1" : "0");
+				var as = data[i].answer.trim();
+				var tmp = data[i].seq + "/" + cr + "/" + as; 
+				sends += tmp.trim();
+				sends += "\n";
+			}
+		}
+	}
+	if (e) {
+		f.txt.value = sends.trim();
+		if (f.txt.value == "") {
+			document.iframe.document.body.innerHTML = "empty data";
+		} else {
+			f.submit();
+		}
+		document.iframe.document.body.style.color = "#fff";
+	}
+}
+
+function setData() {
+	var tmp = list.split("/");
+	var j = 0;
+	for (var i=0; i<tmp.length; i++) {
+		if (tmp[i].trim() == "") continue;
+		else {		//	순번,	시도,	시도회수,	정답여부,	테스트한시간,	북마크여부,	failname,	시작시간,	끝시간,스크립트,대답
+			data[j] = {	seq:j,try:0,count:0,correct:0,timestamp:0,mark:0,fn:"",from:0,to:0,script:"",answer:""};
+			var tmp2 = tmp[i].trim().split("\t");
+			data[j].fn		= tmp2[0].trim();
+			if (tmp2[1] != "") data[j].from = parseInt(tmp2[1]);
+			if (tmp2[2] != "") data[j].to = parseInt(tmp2[2]);
+			data[j].script	= tmp2[3].trim();
+			j++;
+		}
+	}
+}
+
+function attachLeftList() {
+	var leftpanel = document.getElementById("leftpanel");
+	if (!leftpanel) return;
+	if (document.getElementById("leftListView")) return;
+	document.getElementById("defaultListView").remove();
+
+	$.get("left.html", function(template) {
+		$(template).prependTo("#leftpanel");
+		$("[data-role=listview]").listview();
+		$("[data-role=collapsible]").collapsible();
+	}, "html");
+}
+
+function isMobile() {
+	ua = window.navigator.userAgent;
+	isMobile = (/lgtelecom/i.test(ua) 
+			|| /Android/i.test(ua) 
+			|| /blackberry/i.test(ua) 
+			|| /iPhone/i.test(ua) 
+			|| /iPad/i.test(ua) 
+			|| /samsung/i.test(ua) 
+			|| /symbian/i.test(ua) 
+			|| /sony/i.test(ua) 
+			|| /SCH-/i.test(ua) 
+			|| /SPH-/i.test(ua) 
+			|| /nokia/i.test(ua) 
+			|| /bada/i.test(ua) 
+			|| /semc/i.test(ua) 
+			|| /IEMobile/i.test(ua) 
+			|| /Mobile/i.test(ua) 
+			|| /PPC/i.test(ua) 
+			|| /Windows CE/i.test(ua) 
+			|| /Windows Phone/i.test(ua) 
+			|| /webOS/i.test(ua) 
+			|| /Opera Mini/i.test(ua) 
+			|| /Opera Mobi/i.test(ua) 
+			|| /POLARIS/i.test(ua) 
+			|| /SonyEricsson/i.test(ua) 
+			|| /symbos/i.test(ua));
+	return isMobile;
+}
+
+Array.prototype.shuffle = function () {
+	var i = this.length, j, temp;
+	if (i <= 0) return;
+	while (--i) {
+		j = Math.floor( Math.random() * (i + 1) );
+		temp = this[i];
+		this[i] = this[j];
+		this[j] = temp;
+	}
+};
+
+// document.getElementById("id").remove();
+Element.prototype.remove = function() {
+	this.parentElement.removeChild(this);
+}
+// document.getElementsByClassName("class").remove();
+NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+	for(var i = 0, len = this.length; i < len; i++) {
+		if(this[i] && this[i].parentElement) {
+			this[i].parentElement.removeChild(this[i]);
+		}
+	}
+}
---
+++ js/offline_ignore.js
@@ -0,0 +1,4 @@
+function set_ignore_data() {
+	document.getElementById("f2").action = "";
+	document.getElementById("user_email").value = "";
+}
